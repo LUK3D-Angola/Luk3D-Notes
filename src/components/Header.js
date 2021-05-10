ffbb200aa7860ac335a77135cd61cf526ea31a52
LUK3D <filipelukebana@gmail.com>
2021-05-10 12:58:45 +0100

    Atualizei o titulo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header =()=> { 
         return ( 
             <header className="text-gray-300 bg-fg-700 py-2">
-                Luk3D <span className="text-sm">Todo App</span>
+                Luk3D <span className="text-sm">Notes App</span>
             </header>
          );
     }
